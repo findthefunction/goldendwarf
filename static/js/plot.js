e98342b4c7830eae43e815fef9b9cc0fce5e7a53
Simon Adeoye <mymac@Simons-MacBook-Pro.local>
2021-08-02 00:36:40 -0400

    include

--- static/js/plot.js
+++ static/js/plot.js
@@ -1,18 +1,60 @@
-d3.json("items.json").then((data) => {
+d3.json("/data").then((data) => {
     // console.log(data)
-    var tbody = d3.select("tbody")
-    data.forEach(function(x)
-    {
-        // console.log(x)
-        var row = tbody.append("tr")
-        Object.entries(x).forEach(function([key,value]){
-           
-           var cell =  row.append("td")
-           cell.text(value)
+    // var tbody = d3.select("tbody")
+    // data.forEach(function(x)
+    // {
+    //     // console.log(x)
+    //     var row = tbody.append("tr")
+    //     Object.entries(x).forEach(function([key,value]){
+    //        var cell =  row.append("td")
+    //        cell.text(value)
+    //     })
+    // })
 
-
-
-
-        })
-    })
+    console.log(data);
 })
+
+ // 4. Create the trace for the gauge chart.
+
+// 5. Create the layout for the gauge chart.
+var gaugeLayout = { 
+     width: 500, 
+     height: 500, 
+     font: { color: "black", family: "Arial" }
+
+};
+
+
+var data = [
+    {
+      domain: { x: [0, 1], y: [0, 1] },
+      value: 450,
+      title: { text: "Speed" },
+      type: "indicator",
+      mode: "gauge+number",
+      delta: { reference: 400 },
+      gauge: { axis: { range: [null, 500] } }
+    }
+  ];
+  
+  var layout = { width: 600, height: 400 };
+  Plotly.newPlot('gauge', data, layout);
+
+// 6. Use Plotly to plot the gauge data and layout.
+// Plotly.newPlot('gauge', gaugeData, gaugeLayout, { responsive: true }); 
+// Line_Chart 
+var trace1 = {
+  x: [1, 2, 3, 4],
+  y: [10, 15, 13, 17],
+  type: 'line'
+};
+
+var trace2 = {
+  x: [1, 2, 3, 4],
+  y: [16, 5, 11, 9],
+  type: 'line'
+};
+
+var data = [trace1, trace2];
+
+Plotly.newPlot('line', data);
